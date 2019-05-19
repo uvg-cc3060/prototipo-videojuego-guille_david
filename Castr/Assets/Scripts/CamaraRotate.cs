576c46ed5bc1d51a05bf92b5a75529207989b1a9
David Soto <urielsonic@gmail.com>
2019-05-19 17:23:01 -0600

    Se crea la mecanica de movimiento del personaje en 3D

---
+++ Castr/Assets/Scripts/CamaraRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraRotate : MonoBehaviour
+{
+    public float speedV = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotY = Input.GetAxis("Mouse Y") * speedV * Mathf.Deg2Rad;
+        transform.RotateAround(Vector3.right, rotY);
+    }
+}
---
+++ Castr/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 10.0f;
+    public float speedV = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+        transform.Translate(0f, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+        float rotX = Input.GetAxis("Mouse X") * speedV * Mathf.Deg2Rad;
+        transform.RotateAround(Vector3.up, rotX);
+    }
+
+}
